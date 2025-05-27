a1706963d8ef54053d940cc727b25c177a16cb5b
yasmeen Hosam <yhosam@localhost.localdomain>
2025-05-27 12:54:09 +0300

    adding address column to user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_27_092347) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_27_095207) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "DOB"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address", null: false
   end
 end
