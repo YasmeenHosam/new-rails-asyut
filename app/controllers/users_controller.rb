347e690ec533a5eb78a637503c512c485f58e045
yasmeen Hosam <yhosam@localhost.localdomain>
2025-05-27 12:50:51 +0300

    adding user controller and user model with validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,52 @@
 class UsersController < ApplicationController
-    def index
-      # @posts = Post.create(title: "post1")
-      @users= "users from controller"
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      DOB: params[:DOB],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+
+    if @user.save
+      redirect_to @user, notice: "User is successfully created."
+    else
+      render :new
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(
+      name: params[:name],
+      DOB: params[:DOB],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+      redirect_to @user, notice: "User is successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User is successfully deleted."
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
-class user < ApplicationRecord
-end
+class User < ApplicationRecord
+    validates :name, :DOB, :email, :phone_number, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid email" }
+  end
